82f79695fae7dc22078b1a59f33161d6d3f364e6
niu <lnlaoniu@gmail.com>
2023-01-17 17:12:30 +0800

    add check pg

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # StringsAlign.js
 
 基于 [P5.js](http://www.p5js.org/) 的字符串对齐工具。
+
--- stringsAlign.js
+++ stringsAlign.js
@@ -1,43 +1,110 @@
 class StringsAlign {
     constructor() {}
 
-    horizontal(strings, posX, axisX_index, posY, distanceMax, distance, reAxisX) {
+    horizontal(
+        strings,
+        posX,
+        axisX_index,
+        posY,
+        distanceMax,
+        distance,
+        reAxisX,
+        pg
+    ) {
         let axisX_index_int = parseInt(axisX_index);
-        if (axisX_index_int >= strings.length - 1) axisX_index_int = strings.length - 1;
+        if (axisX_index_int >= strings.length - 1)
+            axisX_index_int = strings.length - 1;
         if (axisX_index_int <= 0) axisX_index_int = 0;
-        noStroke();
-        push();
-        translate(posX, posY);
-        for (let i = 0; i < strings.length; i++) {
-            let index_x = i * distance;
-            let pos_x;
-            if (reAxisX) {
-                pos_x = index_x - distance * axisX_index_int;
-            } else {
-                pos_x = index_x - distance * axisX_index_int + distanceMax * axisX_index_int;
+        if (!pg) {
+            noStroke();
+            push();
+            translate(posX, posY);
+            for (let i = 0; i < strings.length; i++) {
+                let index_x = i * distance;
+                let pos_x;
+                if (reAxisX) {
+                    pos_x = index_x - distance * axisX_index_int;
+                } else {
+                    pos_x =
+                        index_x -
+                        distance * axisX_index_int +
+                        distanceMax * axisX_index_int;
+                }
+                text(strings[i], pos_x, 0);
             }
-            text(strings[i], pos_x, 0);
+            pop();
+        } else {
+            pg.noStroke();
+            pg.push();
+            pg.translate(posX, posY);
+            for (let i = 0; i < strings.length; i++) {
+                let index_x = i * distance;
+                let pos_x;
+                if (reAxisX) {
+                    pos_x = index_x - distance * axisX_index_int;
+                } else {
+                    pos_x =
+                        index_x -
+                        distance * axisX_index_int +
+                        distanceMax * axisX_index_int;
+                }
+                pg.text(strings[i], pos_x, 0);
+            }
+            pg.pop();
         }
-        pop();
     }
 
-    vertical(strings, posX, posY, axisY_index, distanceMax, distance, reAxisX) {
+    vertical(
+        strings,
+        posX,
+        posY,
+        axisY_index,
+        distanceMax,
+        distance,
+        reAxisX,
+        pg
+    ) {
         let axisY_index_int = parseInt(axisY_index);
-        if (axisY_index_int >= strings.length - 1) axisY_index_int = strings.length - 1;
+        if (axisY_index_int >= strings.length - 1)
+            axisY_index_int = strings.length - 1;
         if (axisY_index_int <= 0) axisY_index_int = 0;
-        noStroke();
-        push();
-        translate(posX, posY);
-        for (let i = 0; i < strings.length; i++) {
-            let index_y = i * distance;
-            let pos_y;
-            if (reAxisX) {
-                pos_y = index_y - distance * axisY_index_int;
-            } else {
-                pos_y = index_y - distance * axisY_index_int + distanceMax * axisY_index_int;
+
+        if (!pg) {
+            noStroke();
+            push();
+            translate(posX, posY);
+            for (let i = 0; i < strings.length; i++) {
+                let index_y = i * distance;
+                let pos_y;
+                if (reAxisX) {
+                    pos_y = index_y - distance * axisY_index_int;
+                } else {
+                    pos_y =
+                        index_y -
+                        distance * axisY_index_int +
+                        distanceMax * axisY_index_int;
+                }
+                text(strings[i], 0, pos_y);
             }
-            text(strings[i], 0, pos_y);
+            pop();
+        } else {
+            pg.noStroke();
+            pg.push();
+            pg.translate(posX, posY);
+            for (let i = 0; i < strings.length; i++) {
+                let index_y = i * distance;
+                let pos_y;
+                if (reAxisX) {
+                    pos_y = index_y - distance * axisY_index_int;
+                } else {
+                    pos_y =
+                        index_y -
+                        distance * axisY_index_int +
+                        distanceMax * axisY_index_int;
+                }
+                pg.text(strings[i], 0, pos_y);
+            }
+            pg.pop();
         }
-        pop();
     }
 }
